e6135334c5ee9af26626e54227fd3f1591509ae8
Ryan Esteves <ryanplay71@gmail.com>
2023-05-20 14:51:28 -0300

    feat: :art:

---
+++ App.tsx
@@ -0,0 +1,73 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import { styled } from 'nativewind'
+
+// imgs
+import blurBg from './src/assets/luz.png'
+import Stripes from './src/assets/stripes.svg'
+import NlwLogo from './src/assets/nlw-logo.svg'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+const StyledStripes = styled(Stripes)
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        {/* logo */}
+        <NlwLogo />
+
+        {/* hero section */}
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        {/* button */}
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
+      <StatusBar style="light" />
+    </ImageBackground>
+  )
+}
